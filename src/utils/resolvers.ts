9093f9c797078b49a6c4f12a2638fa7ed77390db
xzxpurple2017 <philipdam@hotmail.com>
2024-11-06 11:20:08 -0800

    Format code a bit better

--- src/utils/resolvers.ts
+++ src/utils/resolvers.ts
@@ -4,23 +4,23 @@ import { getAddresses, getAddressByUserPublicId } from '@models/address';
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves users from the "users" table in the database.
 export const resolvers = {
-    Query: {
-      users: async () => {
-        return await getUsers();
-      },
-      user: async (_: any, { publicId }) => {
-        return await getUserByPublicId(publicId);
-      },
-      addresses: async () => {
-        return await getAddresses();
-      },
-      addressByUserPublicId: async (_: any, { publicId }) => {
-        return await getAddressByUserPublicId(publicId);
-      },
+  Query: {
+    users: async () => {
+      return await getUsers();
     },
-    Mutation: {
-      addUser: async (_: any, { email, cognitoId, firstName, lastName, phone }) => {
-        return await createUser(email, cognitoId, firstName, lastName, phone);
-      },
+    user: async (_: any, { publicId }) => {
+      return await getUserByPublicId(publicId);
     },
-  };
+    addresses: async () => {
+      return await getAddresses();
+    },
+    addressByUserPublicId: async (_: any, { publicId }) => {
+      return await getAddressByUserPublicId(publicId);
+    },
+  },
+  Mutation: {
+    addUser: async (_: any, { email, cognitoId, firstName, lastName, phone }) => {
+      return await createUser(email, cognitoId, firstName, lastName, phone);
+    },
+  },
+};
